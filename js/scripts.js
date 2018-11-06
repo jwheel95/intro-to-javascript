6510f9ad88e900732251a54fc4e1b700d16891db
Joe Wheelock <joemwheelock@gmail.com>
2018-11-05 20:38:31 -0800

    completed day 1 takehome

---
+++ js/scripts.js
@@ -0,0 +1,3 @@
+console.log("I'm really looking forward to expanding my horizons in tech and programming specifically. My goal is to get more involved with the Bitwise community and launch a career in programming/development.");
+
+console.log("I have a background in music as a classical bassoonist and a rock guitarist/songwriter. Other than music and programming, some of my interests are hiking, cooking, gaming, and craft beer.")
