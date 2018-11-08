8531a88bfd520c84f042dcfb05f1dd9d01cc961f
Joe Wheelock <joemwheelock@gmail.com>
2018-11-07 20:37:26 -0800

    minor formatting

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,9 @@ const firstName = prompt('What is your first name?');
 const lastName = prompt('What is your last name?');
 
 console.log(firstName);
-
 alert(lastName);
 
 const birthday = new Date(prompt('When were you born? (day, month, and year)'));
-
 confirm(`Is this your birthday? ${birthday}`);
 
 alert(birthday);
