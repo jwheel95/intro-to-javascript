41cf7af04b8535e4cae411b9f07c5867b29be81b
Joe Wheelock <joemwheelock@gmail.com>
2018-11-14 18:14:10 -0800

    added username into responses

--- js/scripts.js
+++ js/scripts.js
@@ -4,34 +4,34 @@ const favColor = prompt(`What is your favorite color, ${name}? Please type red, 
 
 switch (favColor) {
   case 'red':
-    alert("O my love's like a red, red rose.");
+    alert(`Poem for ${name}: O my love's like a red, red rose.`);
     break;
   case 'orange':
-    alert("Golden oranges on the tree, ripening in the sun.");
+    alert(`Poem for ${name}: Golden oranges on the tree, ripening in the sun.`);
     break;
   case 'yellow':
-    alert("The yellow leaves begin to fade, and flutter from the temple elms.");
+    alert(`Poem for ${name}: The yellow leaves begin to fade, and flutter from the temple elms.`);
     break;
   case 'green':
-    alert("Misty's eyes are yellow-green, piercing, endless, gazing, keen...");
+    alert(`Poem for ${name}: Misty's eyes are yellow-green, piercing, endless, gazing, keen...`);
     break;
   case 'blue':
-    alert("Blue is a cool spring day when you play outside.");
+    alert(`Poem for ${name}: Blue is a cool spring day when you play outside.`);
     break;
   case 'pink':
-    alert("A rose is pink by the fountain's brink.");
+    alert(`Poem for ${name}: A rose is pink by the fountain's brink.`);
     break;
   case 'purple':
-    alert("Purple twilight sky, sunset scattering...");
+    alert(`Poem for ${name}: Purple twilight sky, sunset scattering...`);
     break;
   case 'black':
-    alert("Think of the dark as a black park and the moon as a bounced ball.");
+    alert(`Poem for ${name}: Think of the dark as a black park and the moon as a bounced ball.`);
     break;
   case 'white':
-    alert("White is a cool breeze, wind on my cheek.");
+    alert(`Poem for ${name}: White is a cool breeze, wind on my cheek.`);
     break;
   case 'grey':
-    alert("Grey rocks and greyer sea, and rocks along the shore.");
+    alert(`Poem for ${name}: Grey rocks and greyer sea, and rocks along the shore.`);
     break;
   default:
     alert("That wasn't an option. Please refresh the page.");
