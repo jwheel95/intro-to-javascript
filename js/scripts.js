65a74652d7207ee9aa4fdc84d5a70a5ef1a0e500
Joe Wheelock <joemwheelock@gmail.com>
2018-11-19 12:49:36 -0800

    began for loop

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+const chore1 = [prompt("What is one chore you need to do this weekend?")];
+const chore2 = [prompt("What is a second chore you need to do this weekend?")];
+const chore3 = [prompt("What is a third chore you need to do this weekend?")];
+
+const weekend = [chore1, chore2, chore3];
+
+console.log(weekend);
+
+for i
