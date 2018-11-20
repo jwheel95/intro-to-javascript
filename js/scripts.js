4e605be940618f92d9c2ee7a1f63302a88f39f6b
Joe Wheelock <joemwheelock@gmail.com>
2018-11-19 17:32:17 -0800

    finished first for loop

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,11 @@ const chore1 = [prompt("What is one chore you need to do this weekend?")];
 const chore2 = [prompt("What is a second chore you need to do this weekend?")];
 const chore3 = [prompt("What is a third chore you need to do this weekend?")];
 
-const weekend = [chore1, chore2, chore3];
+let weekend = [chore1, chore2, chore3];
 
 console.log(weekend);
 
-for i
+for (let i = 0; i < weekend.length; i++) {
+   weekend[i].push(prompt(`'${weekend[i]}': How many hours will it take to complete this?`));
+
+ }
