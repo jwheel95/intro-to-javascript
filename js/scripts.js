9182b98fb17ea58af00c026a0dc585e64ec408b1
Joe Wheelock <joemwheelock@gmail.com>
2018-11-19 18:35:44 -0800

    completed in class

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,33 @@ const chore1 = [prompt("What is one chore you need to do this weekend?")];
 const chore2 = [prompt("What is a second chore you need to do this weekend?")];
 const chore3 = [prompt("What is a third chore you need to do this weekend?")];
 
-let weekend = [chore1, chore2, chore3];
+const weekend = [chore1, chore2, chore3];
 
 console.log(weekend);
 
 for (let i = 0; i < weekend.length; i++) {
-   weekend[i].push(prompt(`'${weekend[i]}': How many hours will it take to complete this?`));
+   weekend[i].push(+prompt(`'${weekend[i][0]}': How many hours will it take to complete this?`));
 
  }
 
-for (let i = 0; i < weekend.length; i++) {
+ let longestItem = 0;
+ let longestItemIndex = 0;
 
+for (let i = 0; i < weekend.length; i++) {
+  if(weekend[i][1] > longestItem) {
+    longestItem = weekend[i][1];
+  }
+
+  console.log(longestItem);
+  console.log(longestItemIndex);
+}
+
+console.log(weekend);
+console.log('Longest Item: ', weekend.splice([longestItemIndex], 1));
+console.log(weekend);
+
+for(let item of weekend) {
+  item.push('ez-pz');
+
+  alert(item);
 }
