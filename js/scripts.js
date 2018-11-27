60f008a78ab22ed8337be9df59b9fbc1d4b6d94c
Joe Wheelock <joemwheelock@gmail.com>
2018-11-27 04:25:54 -0800

    completed first few requirements

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,16 @@
-function getTitle() {
+js = document.querySelector('script');
+
+startBtn = document.querySelector('#startBtn');
+
+startBtn.addEventListener("click", getTitle => {
   const title = prompt("What is the title of the page?");
   const header = document.createElement('h1');
   header.textContent = title;
-};
-
-document.getElementById("startBtn").addEventListener("click", getTitle());
+  header.className = "text-center";
+  document.body.insertBefore(header, startBtn);
+  const thingsToday = prompt("What are a few things that you did today?");
+  const p = document.createElement('h1');
+  p.textContent = thingsToday;
+  p.className = "text-center col-sm-12";
+  document.body.insertBefore(p, js);
+});
